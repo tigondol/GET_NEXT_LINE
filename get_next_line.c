af123e80962af9299d64fedffa2b7a7d5e8aa20c
TIM <tim@MacBook-Air-de-TIM.local>
2018-06-25 14:57:40 +0200

    get next line

--- get_next_line.c
+++ get_next_line.c
@@ -13,74 +13,56 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		checkerror(int fd, char **dest, char **line)
+int			ft_len(char *str, char c)
 {
-	if (fd == -1)
-		return (-1);
-	if (!*dest)
-	{
-		if (!(*dest = malloc(sizeof(char) * (BUFF_SIZE + 1))))
-			return (-1);
-	}
-	return (0);
+	int i;
+
+	i = 0;
+	if (str == NULL)
+		return (0);
+	while (str[i] && str[i] != c)
+		i++;
+	return (i);
 }
 
-char	*ft_read(int fd, char *dest)
+void		ft_sub(char **line, char **str)
 {
-	int		save;
-	int		ret;
-	char	buff[BUFF_SIZE + 1];
+	char	*s;
 
-	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
-	{
-		buff[ret] = '\0';
-		//printf("||%d\n", ret);
-		save += ret; // POUR FIN DE DEST
-		//printf("__\n%d__\n", save);
-		dest = ft_join(dest, buff);
-		//ft_putendl(dest);
-		//ft_putendl("\n________________________________/\\");
-	}
-	//dest[save] = '\0'; // POUR FIN DE DEST
-	return (dest);
+	s = NULL;
+	*line = ft_strsub(*str, 0, ft_len(*str, '\n'));
+	s = ft_strsub(*str, ft_len(*str, '\n') + 1, 
+		ft_strlen(&str[0][ft_len(*str, '\n')]));
+	ft_strdel(str);
+	*str = s;
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	int 			i;
-	static	char	*dest;
+	static	char	*str[OPEN_MAX];
+	char 			buff[BUFF_SIZE + 1];
+	char 			*tmp;
 
-	i = 0;
+	ret = 0;
+	if (line == NULL || fd < 0 || BUFF_SIZE < 0 ||	fd > OPEN_MAX)
+		return (-1);
+	if (!str[fd])
+		str[fd] = ft_strnew(1);
 	if (checkerror(fd, &dest, line) == -1)
 		return (-1);
-	//if (*dest)
-		//ft_strcpy(*line, dest);
-	dest = ft_read(fd, dest);
-	ft_putendl("_________________________________");
-	ft_putendl(dest);
-	// printf("++\n%s\n++\n", dest);
-	if (dest[i])
+	while (!(ft_strchr(str[fd], '\n')))
 	{
-		// printf("lol");
-		i = 0;
-		while (dest[i] != '\n' && dest[i])
-			i++;
-		if (i == 0)
-		{
-			(*line) = ft_strdup("");
-			dest = &dest[i + 1];
-		}
-		else
-		{
-			(*line) = ft_strsub(dest, 0, i);
-			dest = &dest[i + 1];
-			ft_strdel(&dest[i]);
-	//printf("[\n%s\n]\n", *line);
-		}
-		return (1);
+		ret = read(fd, buff, BUFF_SIZE);
+		if (ret == 0)
+			break ;
+		buff[ret] = '\0';
+		tmp = ft_strjoin(str[fd], buff);
+		ft_strdel(&str[fd]);
+		str[fd] = tmp;
 	}
-	else
-		(*line) = ft_strdup("");
-	//printf("TEST");
-	return (0);
+	if (ft_strlen(str[fd]) == 0)
+		return (0);
+	ft_sub(line, &str[fd]);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -14,10 +14,10 @@
 # define GET_NEXT_LINE_H
 
 # include <fcntl.h>
-# include "./Libft/libft.h"
+# include "Libft/libft.h"
 # define BUFF_SIZE 10
+# define OPEN_MAX 256
 
 int		get_next_line(const int fd, char **line);
-char	*ft_join(char *s1, char *s2);
 
 #endif
